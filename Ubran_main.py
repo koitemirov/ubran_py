3a93689a21d4ce63f4f53b42e4d516e31bec36c8
koitemirov <a.koitemirov@gmail.com>
2025-01-18 16:17:21 +0300

    18/01/25 - Задания 1го урока

--- Ubran_main.py
+++ Ubran_main.py
@@ -1,2 +1,11 @@
 ##1st program
 print(9**0.5*5)
+##2st program
+print(9.99>9.98 and 1000 != 1001)
+##3st program
+print(2*2+2)
+print(2*(2+2))
+print ((2*2+2)==(2*(2+2)))
+##4th program
+a= ('123.456')
+print(int(float(a)*10)%10)
